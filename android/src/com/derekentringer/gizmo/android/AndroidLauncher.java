55a001a0d2b0a48f4532b4fb36341908028d37dd
Derek Entringer <dentringer@gmail.com>
2015-06-27 11:07:55 -0500

    initial new project setup

---
+++ android/src/com/derekentringer/gizmo/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.derekentringer.gizmo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.derekentringer.gizmo.Gizmo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Gizmo(), config);
+	}
+}
