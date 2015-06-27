0350cb310a3b6d808a385d4565778ffd2c96375b
Derek Entringer <dentringer@gmail.com>
2015-06-27 11:15:30 -0500

    updated android manifest; updated desktop launcher; added banner

--- desktop/src/com/derekentringer/gizmo/desktop/DesktopLauncher.java
+++ desktop/src/com/derekentringer/gizmo/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.derekentringer.gizmo.Gizmo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.width = Constants.GAME_WIDTH;
+        //config.height = Constants.GAME_HEIGHT;
+        config.vSyncEnabled = true;
 		new LwjglApplication(new Gizmo(), config);
 	}
 }
