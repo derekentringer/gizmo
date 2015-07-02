a6b541974c3e5ceaf50afd27552e16b76976a619
Derek Entringer <dentringer@gmail.com>
2015-07-01 20:56:40 -0500

    added a few comments

--- core/src/com/derekentringer/gizmo/screen/GameScreen.java
+++ core/src/com/derekentringer/gizmo/screen/GameScreen.java
@@ -77,6 +77,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
+        //TODO not sure how to destroy
+        //the world for Android successfully
+        stage.dispose();
     }
 
 }
