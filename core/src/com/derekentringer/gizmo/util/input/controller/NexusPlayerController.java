e962218dea7f91f8d19e42d01eb5accea2868316
Derek Entringer <dentringer@gmail.com>
2015-07-02 19:58:54 -0500

    fixed nexus Y button bug

--- core/src/com/derekentringer/gizmo/util/input/controller/NexusPlayerController.java
+++ core/src/com/derekentringer/gizmo/util/input/controller/NexusPlayerController.java
@@ -5,7 +5,7 @@ public class NexusPlayerController extends BaseController {
     public static final int BUTTON_X = 99;
     public static final int BUTTON_A = 96;
     public static final int BUTTON_B = 97;
-    public static final int BUTTON_Y = 98;
+    public static final int BUTTON_Y = 100;
 
     public static final int RB = 103;
     public static final int LB = 102;
