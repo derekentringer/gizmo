f0ea1f217e6c81aa2fb0a61eab02fa9694d4479b
Derek Entringer <dentringer@gmail.com>
2015-07-03 19:28:55 -0500

    added level class

---
+++ core/src/com/derekentringer/gizmo/level/Level.java
@@ -0,0 +1,64 @@
+package com.derekentringer.gizmo.level;
+
+public class Level {
+
+    private int sLevelInt;
+    private String sLevelMap;
+    private String sLevelMidMap;
+    private String sLevelBackMap;
+    private int sXpos;
+    private int sYpos;
+
+    public Level() {
+        super();
+    }
+
+    public int getLevelInt() {
+        return sLevelInt;
+    }
+
+    public void setLevelInt(int levelInt) {
+        sLevelInt = levelInt;
+    }
+
+    public String getLevelMap() {
+        return sLevelMap;
+    }
+
+    public void setLevelMap(String levelMap) {
+        sLevelMap = levelMap;
+    }
+
+    public String getsLevelMidMap() {
+        return sLevelMidMap;
+    }
+
+    public void setsLevelMidMap(String levelMidMap) {
+        sLevelMidMap = levelMidMap;
+    }
+
+    public String getsLevelBackMap() {
+        return sLevelBackMap;
+    }
+
+    public void setsLevelBackMap(String levelBackMap) {
+        sLevelBackMap = levelBackMap;
+    }
+
+    public int getXpos() {
+        return sXpos;
+    }
+
+    public void setsXpos(int xPos) {
+        sXpos = xPos;
+    }
+
+    public int getYpos() {
+        return sYpos;
+    }
+
+    public void setsYpos(int yPos) {
+        sYpos = yPos;
+    }
+
+}
