73069f7f3d2a7c4652132d1c274e00bd8c78b0d8
Derek Entringer <dentringer@gmail.com>
2015-08-16 17:01:31 -0500

    added glClearColor

--- core/src/com/derekentringer/gizmo/components/screen/GameScreen.java
+++ core/src/com/derekentringer/gizmo/components/screen/GameScreen.java
@@ -39,6 +39,7 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         //clear the screen
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //set the viewport
