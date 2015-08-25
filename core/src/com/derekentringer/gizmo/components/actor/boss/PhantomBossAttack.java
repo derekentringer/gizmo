0f0649cb78031a979b87b313731ba4f3cb00f272
Derek Entringer <dentringer@gmail.com>
2015-08-24 21:43:01 -0500

    fireball timer adjustment

--- core/src/com/derekentringer/gizmo/components/actor/boss/PhantomBossAttack.java
+++ core/src/com/derekentringer/gizmo/components/actor/boss/PhantomBossAttack.java
@@ -20,7 +20,7 @@ public class PhantomBossAttack extends Stage {
     private ArrayList<IPhantomBossAttack> listeners = new ArrayList<IPhantomBossAttack>();
 
     private static final int PHANTOM_ATTACK_DELAY = 4000;
-    private static final int FIREBALL_ATTACK_DELAY = 6000;
+    private static final int FIREBALL_ATTACK_DELAY = 2000;
 
     private static final int SHAKE_DELAY = 2000;
     private static final int SHAKE_LENGTH = 4000;
