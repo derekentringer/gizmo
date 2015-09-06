54765f1f0636c498ac9a2422aa04ba4573a6cc8a
Derek Entringer <dentringer@gmail.com>
2015-09-06 18:13:15 -0500

    renamed boomerang model

---
+++ core/src/com/derekentringer/gizmo/model/player/item/BoomerangModel.java
@@ -0,0 +1,21 @@
+package com.derekentringer.gizmo.model.player.item;
+
+import com.derekentringer.gizmo.model.BaseModelType;
+
+public class BoomerangModel extends BaseItemModel {
+
+    public static final String BOOMERANG_WOOD = "BOOMERANG_WOOD";
+
+    private int mHealthDamage = 2;
+
+    public BoomerangModel() {
+        super();
+        mBaseModelType = BaseModelType.PLAYER_ITEM;
+    }
+
+    @Override
+    public int getHealthDamage() {
+        return mHealthDamage;
+    }
+
+}
