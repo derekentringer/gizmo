8ac2b7bb4aa47f85d17b666bc33dcabab71bba15
Derek Entringer <dentringer@gmail.com>
2015-09-13 22:04:15 -0500

    added lava

---
+++ core/src/com/derekentringer/gizmo/model/structure/LavaModel.java
@@ -0,0 +1,15 @@
+package com.derekentringer.gizmo.model.structure;
+
+import com.derekentringer.gizmo.model.BaseModel;
+import com.derekentringer.gizmo.model.BaseModelType;
+
+public class LavaModel extends BaseModel {
+
+    public static final String LAVA = "LAVA";
+
+    public LavaModel() {
+        super();
+        mBaseModelType = BaseModelType.LAVA;
+    }
+
+}
