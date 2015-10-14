47fea732624559d698d57f7107f400e82b4915fb
Derek Entringer <dentringer@gmail.com>
2015-10-13 19:42:10 -0500

    cleanup PLAYER_ATTACK_ENTITY

--- core/src/com/derekentringer/gizmo/settings/Constants.java
+++ core/src/com/derekentringer/gizmo/settings/Constants.java
@@ -11,8 +11,7 @@ public class Constants {
     public static final short PLAYER_ENTITY = 0x1 << 1;
     public static final short PLAYER_ATTACK_ENTITY = 0x1<< 2;
     public static final short ENEMY_ENTITY = 0x1 << 3;
-    public static final short ENEMY_ATTACK_ENTITY = 0x1 << 4;
-    public static final short DROP_ENTITY = 0x1 << 5;
+    public static final short DROP_ENTITY = 0x1 << 4;
 
     // debugging
     public static final boolean IS_DEBUG = true;
