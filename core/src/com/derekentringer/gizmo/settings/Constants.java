efb33ed5e8c3ad5b266f0da4b928756d055c6f71
Derek Entringer <dentringer@gmail.com>
2016-04-06 20:29:17 -0500

    versioning updated

--- core/src/com/derekentringer/gizmo/settings/Constants.java
+++ core/src/com/derekentringer/gizmo/settings/Constants.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 
 public class Constants {
 
-    public static final String VERSION = "ALPHA-0.0.1";
+    public static final String VERSION = "0.0.1";
 
     // categoryBits declares what entity it is
     // maskBits declares which entities collide with
